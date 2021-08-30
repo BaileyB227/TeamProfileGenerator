702b1a1ec5095101135a2892e15a4891b30ad660
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-08-30 12:14:27 -0500

    fixed employee tests

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -42,8 +42,8 @@ describe("Employee", () => {
         
     describe("getEmail", () => {
         it("Uses getEmail() to get the email", () => {
-            const testValue = "test@test.com";
-            const e = new Employee("Foo", 1, testValue);
+            const testValue = "bailey@email.com";
+            const e = new Employee("Bailey", 1, testValue);
             expect(e.getEmail()).toBe(testValue);
         });
     });
@@ -51,7 +51,7 @@ describe("Employee", () => {
     describe("getRole", () => {
         it("Uses getRole() to get the role, should be Employee", () => {
             const testValue = "Employee";
-            const e = new Employee("Alice", 1, "test@test.com");
+            const e = new Employee("Bailey", 1, "bailey@email.com");
             expect(e.getRole()).toBe(testValue);
         });
     });
