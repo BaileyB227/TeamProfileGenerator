5dd22ecaa6217f3952ffe34bf1f5944036b5b108
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-08-30 12:43:55 -0500

    fixed intern details, manager tests working

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -6,13 +6,13 @@ test("Sets school", () => {
   expect(e.school).toBe(testValue);
 });
 
-test("getRole() should return \"Intern\"", () => {
+test("Uses getRole() to get the role, should be Intern", () => {
   const testValue = "Intern";
   const e = new Intern("Bailey", 1, "bailey@email.com", "SMU");
   expect(e.getRole()).toBe(testValue);
 });
 
-test("Can get school via getSchool()", () => {
+test("Uses getSchool() to get the school", () => {
   const testValue = "SMU";
   const e = new Intern("Bailey", 1, "bailey@email.com", testValue);
   expect(e.getSchool()).toBe(testValue);
--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,1 +1,19 @@
-const Manager = require("../lib/Employee");
+const Manager = require("../lib/Manager");
+
+test("Sets office number", () => {
+    const testValue = 100;
+    const e = new Manager("Bailey", 1, "bailey@email.com", testValue);
+    expect(e.officeNumber).toBe(testValue);
+});
+  
+test("getRole() should return \"Manager\"", () => {
+    const testValue = "Manager";
+    const e = new Manager("Bailey", 1, "bailey@email.com", 100);
+    expect(e.getRole()).toBe(testValue);
+});
+  
+test("Uses getOfficeNumber() to get the office number", () => {
+    const testValue = 100;
+    const e = new Manager("Bailey", 1, "bailey@email.com", testValue);
+    expect(e.getOfficeNumber()).toBe(testValue);
+});
