ac32142354e93aad124e638fcd15ee457cfb92c5
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-08-30 12:38:52 -0500

    intern tests working

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,1 +1,19 @@
 const Intern = require("../lib/Intern");
+
+test("Sets school", () => {
+  const testValue = "SMU";
+  const e = new Intern("Bailey", 1, "bailey@email.com", testValue);
+  expect(e.school).toBe(testValue);
+});
+
+test("getRole() should return \"Intern\"", () => {
+  const testValue = "Intern";
+  const e = new Intern("Bailey", 1, "bailey@email.com", "SMU");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get school via getSchool()", () => {
+  const testValue = "SMU";
+  const e = new Intern("Bailey", 1, "bailey@email.com", testValue);
+  expect(e.getSchool()).toBe(testValue);
+});
