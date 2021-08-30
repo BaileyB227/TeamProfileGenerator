1675a5875a8f3e4f2c5e6bd8ef0bc4b02a51d557
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-08-30 13:06:30 -0500

    inquirer started, working

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
-const Employee = require("./lib/Employee");
-const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+function addTeamMember() {
+    inquirer.prompt([{
+        type: "input",
+        message: "What is the team members name?",
+        name: "name"
+    },
+    {
+        type: "list",
+        message: "What is the team members role?",
+        name: "role",
+        choices: [
+            "Intern",
+            "Engineer",
+            "Manager"
+        ]
+    },
+    {
+        type: "input",
+        message: "What is the team members ID?",
+        name: "ID"
+    },
+    {
+        type: "input",
+        message: "What is the team members Email?",
+        name: "Email"
+    }
+])
+}
+
+addTeamMember();
